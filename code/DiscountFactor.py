b770a369ef2145a19ade7fb3454f9a3ef4068b4d
arstep-ds <arstep.ds@gmail.com>
2023-11-19 16:49:40 +0100

    discount factor calculation

--- code/DiscountFactor.py
+++ code/DiscountFactor.py
@@ -1,3 +1,12 @@
+import math
+
 def disc_factor(r, m, t):
     """Calculate discount factor, given rate r, freq m and time t"""
     return 1 / ((1+(r/m))**(m*t))
+
+def disc_factor_simple_rate(r_s, t):
+    """Calculate discount factor, given simple rate r_s and time t"""
+    return 1 / (1+r_s*t)
+
+def disc_factor_cont_rate(r_c, t):
+    return math.exp(-r_c*t)
